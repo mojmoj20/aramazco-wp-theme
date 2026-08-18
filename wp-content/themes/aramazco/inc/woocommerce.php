2f06d2251453648d7c2e8bf076db01c5d7c94e19
mojmoj20 <55236319+mojmoj20@users.noreply.github.com>
2026-08-18 08:37:19 +0330

    Style WooCommerce My Account / Login to match Aramazco dark theme

--- wp-content/themes/aramazco/inc/woocommerce.php
+++ wp-content/themes/aramazco/inc/woocommerce.php
@@ -12,3 +12,13 @@ add_action('after_setup_theme', function (): void {
 	add_theme_support('wc-product-gallery-lightbox');
 	add_theme_support('wc-product-gallery-slider');
 });
+
+/**
+ * Add a body class when WooCommerce is active.
+ */
+add_filter('body_class', function (array $classes): array {
+	if (class_exists('WooCommerce')) {
+		$classes[] = 'aramazco-has-woocommerce';
+	}
+	return $classes;
+});
