b1d29c306919ad3d52c2499402ed690f5613146e
mojmoj20 <55236319+mojmoj20@users.noreply.github.com>
2026-08-18 10:48:54 +0330

    Refine shop grid + square product images (less tall cards)

--- wp-content/themes/aramazco/functions.php
+++ wp-content/themes/aramazco/functions.php
@@ -9,7 +9,7 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
-define('ARAMAZCO_THEME_VERSION', '0.1.2');
+define('ARAMAZCO_THEME_VERSION', '0.1.3');
 define('ARAMAZCO_THEME_DIR', get_template_directory());
 define('ARAMAZCO_THEME_URI', get_template_directory_uri());
 
